402339cc20b2622a15445e1498d8529a23f27a94
Alberto Piras <alberto.piras@inpeco.com>
2018-08-19 12:22:35 +0200

    Update main app component and home component

---
+++ angular-starter-kit/src/app/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  onLogout(){
+    this.authService.logout();
+  }
+
+}
