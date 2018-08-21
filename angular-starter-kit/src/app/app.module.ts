4b7f62fb5cb6e56ba82f376ba7061266714a51da
Alberto Piras <alberto.piras@inpeco.com>
2018-08-21 11:27:13 +0200

    Update app module with all dependencies

--- angular-starter-kit/src/app/app.module.ts
+++ angular-starter-kit/src/app/app.module.ts
@@ -1,16 +1,95 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth-guard.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Router } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material.module'
+import { ToastrModule } from 'ngx-toastr';
+import { ErrorInterceptor } from './httpErrorInterceptor.service';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { Observable, throwError } from 'rxjs';
+import 'rxjs/operators';
+import { RequestsInterceptor } from './httpRequestsInterceptor.service';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { DashboardService } from './dashboard/dashboard.service';
+import { CalendarModule } from 'primeng/calendar';
+import { NavComponent } from './nav/nav.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { StatusService } from './status.service';
+import { NGPrimeModule } from './primeng.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ErrorPageComponent,
+    DashboardComponent,
+    NavComponent,
+    SidebarComponent
   ],
+  entryComponents: [],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    MaterialModule,
+    ChartsModule,
+    NGPrimeModule,
+    CalendarModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'en-US'
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: RequestsInterceptor,
+      multi: true
+    },
+    DashboardService,
+    AuthService,
+    StatusService,
+    AuthGuard
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
